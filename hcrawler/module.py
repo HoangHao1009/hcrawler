2c58007c6dd9ed1b342f8f46f060e4f520c486db
Hà Hoàng Hảo <112957852+HoangHao1009@users.noreply.github.com>
2024-02-03 17:03:19 +0700

    Update module.py

--- hcrawler/module.py
+++ hcrawler/module.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, TimeoutException, StaleElementReferenceException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementClickInterceptedException, TimeoutException
 from time import sleep
 import threading
 from queue import Queue
@@ -33,50 +33,58 @@ class func:
             info_elems = driver.find_elements(By.CSS_SELECTOR, elem)
             title_elem, detail_info_elem, describe_elem = full_info_elem[0], full_info_elem[1], full_info_elem[2]
             for i in info_elems:
+                info, describe, seller, seller_star, seller_reviews_quantity, seller_follow = None, None, None, None, None, None
                 try:
                     title = i.find_element(By.CSS_SELECTOR, title_elem)
                     title = title.text
-                except (NoSuchElementException, StaleElementReferenceException):
-                    title = np.nan
+                except:
+                    title = None
                 if title == 'Thông tin chi tiết':
                     try:
                         info_row = i.find_elements(By.CSS_SELECTOR, detail_info_elem)
                         info = [i.text.split('\n') for i in info_row]
-                    except NoSuchElementException:
-                        info = np.nan
+                    except Exception as e:
+                        print(f'info except: {threading.current_thread().name} - {type(e).__name__}')
+                        info = None
                         print('detail_info nan')
                 elif title == 'Mô tả sản phẩm':
                     try:
                         describe = i.find_element(By.CSS_SELECTOR, describe_elem).text
-                    except NoSuchElementException:
-                        describe = np.nan
+                    except Exception as e:
+                        print(f'describe except: {threading.current_thread().name} - {type(e).__name__}')
+                        describe = None
                         print('describe nan')
                 elif title == 'Thông tin nhà bán':
                     try:
                         seller = i.find_element(By.CSS_SELECTOR, '.seller-name').text.split(' ')[0]
-                    except NoSuchElementException:
-                        seller = np.nan
+                    except Exception as e:
+                        print(f'seller except: {threading.current_thread().name} - {type(e).__name__}')
+                        seller = None
                         print('seller nan')
                     try:
                         seller_evaluation_elems = i.find_element(By.CSS_SELECTOR, '.item.review')
-                    except NoSuchElementException:
-                        seller_evaluation_elems = np.nan
+                    except Exception as e:
+                        print(f'seller eval except: {threading.current_thread().name} - {type(e).__name__}')
+                        seller_evaluation_elems = None
                         print('seller_evaluation nan')
                     try:
                         seller_star = seller_evaluation_elems.find_element(By.CSS_SELECTOR, '.title').text
-                    except NoSuchElementException:
-                        seller_star = np.nan
+                    except Exception as e:
+                        print(f'seller star except: {threading.current_thread().name} - {type(e).__name__}')
+                        seller_star = None
                         print('seller_star nan')
                     try:
                         seller_reviews_quantity = seller_evaluation_elems.find_element(By.CSS_SELECTOR, '.sub-title').text
-                    except NoSuchElementException:
-                        seller_reviews_quantity = np.nan
+                    except Exception as e:
+                        print(f'seller review except: {threading.current_thread().name} - {type(e).__name__}')
+                        seller_reviews_quantity = None
                         print('seller_reviews_quantity nan') 
                     try:
                         seller_follow = i.find_element(By.CSS_SELECTOR, '.item.normal .title').text
-                    except NoSuchElementException:
-                        seller_follow = np.nan
-                        print('seller_follow nan')   
+                    except Exception as e:
+                        print(f'seller follow except: {threading.current_thread().name} - {type(e).__name__}')
+                        seller_follow = None
+                        print('seller_follow nan')
             output = (info, describe, seller, seller_star, seller_reviews_quantity, seller_follow)
         else:
             print('no output')
@@ -94,11 +102,12 @@ class func:
             for _ in range(scroll_iters):
                 driver.execute_script("window.scrollBy(0, arguments[0]);", scroll_amount)
                 sleep(scroll_interval)
-        except TimeoutException:
-            sleep(30)
-            for _ in range(scroll_iters):
-                driver.execute_script("window.scrollBy(0, arguments[0]);", scroll_amount)
-                sleep(scroll_interval)
+        except Exception as e:
+            print(f'scroll except: {threading.current_thread().name} - {type(e).__name__}')
+            sleep(10)
+            func.scroll(driver, scroll_iters = 10, scroll_amount = 300, scroll_interval = 0.2)
+            print(f'Try to re-scroll sucessfully')
+
 
     @staticmethod
     def load_multi_page(driver, n, link = str):
@@ -121,15 +130,15 @@ class func:
         try:
             func.wait(driver, 30, prod_link_elem)
             pl = func.get_elem(driver, prod_link_elem, elem_type = 'prod_link')
-            print('Take Product links succesfully')
-        except TimeoutException:
-            print('First Product link not exist')
+            print(f'{threading.current_thread().name} - Take Product links succesfully')
+        except Exception as e:
+            print(f'{threading.current_thread().name} - First Product link not exist')
             try:
                 func.wait(driver, 30, preventive_prod_link_elem)
                 pl = func.get_elem(driver, preventive_prod_link_elem, elem_type = 'prod_link')
-                print('Take prevent Product link succesfully')
-            except:
-                print('Prevent Product link not exist also')
+                print(f'{threading.current_thread().name} - Take prevent Product link succesfully')
+            except Exception as e:
+                print(f'{threading.current_thread().name} - Prevent Product link not exist also')
                 pl = []
         page_features = []
         for prod_link in pl:
@@ -142,52 +151,65 @@ class func:
             try:
                 elem_to_wait = func.wait(driver, 10, extend_page_elem)
                 elem_to_wait.click()
-            except (NoSuchElementException, TimeoutException):
-                print('Expand page not exist')
-            
+            except ElementClickInterceptedException:
+                print(f'click except: {threading.current_thread().name}')
+                try:
+                    func.scroll(driver, 2)
+                    elem_to_wait.click()
+                except Exception as e:
+                    print(f'Cant not click: {threading.current_thread().name} - {type(e).__name__}')
+            except TimeoutException:
+                print(f'Expand page not exist')
             #cate
             try:
                 func.wait(driver, 2, category_bar_elem)
                 cate = func.get_elem(driver, category_bar_elem, elem_type = 'cate')
-            except (NoSuchElementException, TimeoutException):
-                cate = np.nan
+            except Exception as e:
+                print(f'cate except: {threading.current_thread().name} - {type(e).__name__}')
+                cate = None
                 print('cate nan')
             #img
             try:
                 func.wait(driver, 2, image_elem)
                 img = func.get_elem(driver, image_elem, elem_type = 'img')
-            except (NoSuchElementException, TimeoutException):
-                img = np.nan
+            except Exception as e:
+                print(f'img except: {threading.current_thread().name} - {type(e).__name__}')
+                img = None
                 print('img nan')
             #price
             try:
                 func.wait(driver, 2, price_elem)
                 price = func.get_elem(driver, price_elem, elem_type = 'price')
-            except (NoSuchElementException, TimeoutException):
-                price = np.nan
+            except Exception as e:
+                print(f'price except: {threading.current_thread().name} - {type(e).__name__}')
+                price = None
                 print('price nan')
             #discount
             try:
                 func.wait(driver, 2, discount_elem)
                 discount = func.get_elem(driver, discount_elem, elem_type = 'discount')
-            except (NoSuchElementException, TimeoutException):
-                discount = np.nan
+            except Exception as e:
+                print(f'discount except: {threading.current_thread().name} - {type(e).__name__}')
+                discount = None
                 print('discount nan')
             #rating
             try:
                 func.wait(driver, 2, rating_elem)
                 rating = func.get_elem(driver, rating_elem, elem_type = 'rating')
-            except (NoSuchElementException, TimeoutException):
-                rating = np.nan
+            except Exception as e:
+                print(f'rating except: {threading.current_thread().name} - {type(e).__name__}')
+                rating = None
                 print('rating nan')
             #sales quant
             try:
                 func.wait(driver, 2, sales_quantity_elem)
                 sale_q = func.get_elem(driver, sales_quantity_elem, elem_type = 'sale_q')
-            except (NoSuchElementException, TimeoutException):
-                sale_q = np.nan
+            except Exception as e:
+                print(f'sale_q except: {threading.current_thread().name} - {type(e).__name__}')
+                sale_q = None
                 print('sale_q nan')
-            #full_info            
+            #full_info
+                
             info, describe, seller, seller_star, seller_reviews_quantity, seller_follow = func.get_elem(driver, 
                                                                                                         info_elem, 
                                                                                                         (title_elem, detail_info_elem, describe_elem), 
@@ -224,7 +246,7 @@ class func:
             result = pd.Series([float(i.replace('%', ''))/ 100 if isinstance(i, str) else i for i in col], name = col.name)
 
         elif col.name == 'sale_quantity':
-            result = col.apply(lambda x: float(re.sub(r'[^0-9]', '', x) if not pd.isna(x) else np.nan))
+            result = col.apply(lambda x: float(re.sub(r'[^0-9]', '', x) if not pd.isna(x) else None))
 
         elif col.name == 'rating':
             rating_star = []
@@ -252,13 +274,13 @@ class func:
                     info_i.append(v[0])
                 lack = [i for i in di.keys() if i not in info_i]
                 for l in lack:
-                    i.append([l, np.nan])
+                    i.append([l, None])
             for i in col:
                 for v in i:
                     try:
                         di[v[0]].append(v[1])
                     except:
-                        di[v[0]].append(np.nan)
+                        di[v[0]].append(None)
 
             result = pd.DataFrame(di)
 
@@ -319,9 +341,12 @@ class TikiCrawler(func):
 
     def run(self):
         threads = []
+        i = 1
+        thread_name = f'thread {i}'
         for driver in self.drivers:
             print('--Running--')
-            t = threading.Thread(target = func.get_data, 
+            t = threading.Thread(name = thread_name,
+                                 target = func.get_data, 
                                  args = (driver, self.que, self.prod_link_elem, 
                                          self.category_bar_elem, self.image_elem, 
                                          self.price_elem, self.discount_elem,
@@ -329,9 +354,10 @@ class TikiCrawler(func):
                                          self.info_elem, self.detail_info_elem, 
                                          self.describe_elem, self.extend_page_elem,
                                          self.title_elem, self.preventive_prod_link_elem))
-            t.start()
             threads.append(t)
-        
+            i += 1
+        for t in threads:
+            t.start()
         for t in threads:
             t.join()
         results = []
@@ -363,11 +389,15 @@ class TikiCrawler(func):
             data.append(part)
         self.wrangled_data = pd.concat(data, axis = 1)
 
-    def sub_crawler(self, sub_link_elem):
+    def sub_crawler(self, sub_link_elem, preventive_sub_link_elem):
         temp_drive = webdriver.Chrome()
         temp_drive.get(self.root_link)
-        func.wait(temp_drive, 10, sub_link_elem)
-        sub_link_elem = temp_drive.find_elements(By.CSS_SELECTOR, sub_link_elem)
+        try:
+            func.wait(temp_drive, 10, sub_link_elem)
+            sub_link_elem = temp_drive.find_elements(By.CSS_SELECTOR, sub_link_elem)
+        except:
+            func.wait(temp_drive, 10, preventive_sub_link_elem)
+            sub_link_elem = temp_drive.find_elements(By.CSS_SELECTOR, preventive_sub_link_elem)
         info = [(i.text, i.get_attribute('href')) for i in sub_link_elem]
         sub_crawler = {}
         for name, link in info:
@@ -382,6 +412,7 @@ class TikiCrawler(func):
                 self.describe_elem, self.extend_page_elem,
                 self.title_elem, self.preventive_prod_link_elem
             )
+        temp_drive.close()
         return sub_crawler
     
     def close(self):
