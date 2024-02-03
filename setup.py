117ad18a96fc511f624161a8bc46278762465a87
Hà Hoàng Hảo <112957852+HoangHao1009@users.noreply.github.com>
2024-02-03 14:10:23 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name = 'hcrawler',
+  version = '0.1',
+  author = 'Ha Hoang Hao',
+  packages = ['hcrawler'],
+  description = 'Crawl Tiki web data using selenium',
+  install_requires = ['pandas', 'numpy', 'selenium', 'threading', 'queue', 're', 'ast', 'pickle']
+)
